59a69a696554c7d38dc03435eb57b3740db35cb5
abresee <bresee.al@gmail.com>
2014-01-18 22:26:14 -0500

    really ugly hack to make pkg-config work

--- cython/gst_test/setup.py
+++ cython/gst_test/setup.py
@@ -1,13 +1,28 @@
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
+from subprocess import check_output
+from re import search
 
-setup(
+lib = "gstreamer-1.0"
+
+def fsh(arg):
+    r=[]
+    for i in str(check_output(arg)).split():
+        m=search("-[Il]([\w\d/.-]+)",i)
+        if m:
+            print("hit")
+            r.append(m.group(1))
+    print(r)
+    return r
+
+cflags = fsh(["pkg-config",lib,"--cflags"])
+libs = fsh(["pkg-config",lib,"--libs"])
+
+setup (
     cmdclass = {'build_ext': build_ext},
     ext_modules = [Extension("cygst",
                    sources=["gst_test.pyx", "gst_test2.c"],
-                   include_dirs=["/usr/include/gstreamer-1.0", 
-                                 "/usr/include/glib-2.0",
-                                 "/usr/lib/x86_64-linux-gnu/glib-2.0/include"],
-                   libraries=["gstreamer-1.0","gobject-2.0","glib-2.0"])]
+                   include_dirs=cflags,             
+                   libraries=libs)]
 )
